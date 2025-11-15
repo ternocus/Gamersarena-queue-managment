5394ea9f76d09cf2767ed33d115dd6d3d9d632f7
Alessandro Pioggia <apioggia@qubicaamf.com>
2025-11-15 17:38:28 +0100

    add new endpoint for check xml file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import fetch from "node-fetch";
+import csvParser from "csv-parser";
 
 const app = express();
 app.use(cors());
@@ -61,6 +63,52 @@ app.post("/api/queue/reset", (req, res) => {
 
   io.emit("resetQueue", { game, currentPlayer: 0, reset: true });
   res.json({ game, currentPlayer: 0, TotalPlayers: 0 });
+});
+
+// /api/sheet/search?nome=Mario&cognome=Rossi
+app.get("/api/sheet/search", async (req, res) => {
+  const { nome, cognome } = req.query;
+
+  if (!nome || !cognome) {
+    return res.status(400).json({ error: "Parametri richiesti: nome, cognome" });
+  }
+
+  const oggi = new Date();
+  const day = String(oggi.getDate()).padStart(2, "0");
+  const month = String(oggi.getMonth() + 1).padStart(2, "0");
+  const year = oggi.getFullYear();
+  const dataOggi = `${day}/${month}/${year}`;
+
+  try {
+    const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTWjJA3OafqoG2ytwQJEUxSUypz-syfOVSJrK-pxvWmCEHjugcIxXcqI7aVhCwPQoqFL3CXY5eSQhey/pub?gid=336746532&single=true&output=csv";
+
+    const response = await fetch(url);
+
+    const rows = [];
+    response.body
+      .pipe(csvParser())
+      .on("data", row => rows.push(row))
+      .on("end", () => {
+        const target = rows.find(r => {
+          const data = r["Informazioni cronologiche"]?.trim();
+          const nomeCsv = r["NOME"]?.trim().toLowerCase();
+          const cognomeCsv = r["COGNOME"]?.trim().toLowerCase();
+
+          return (
+            nomeCsv === nome.trim().toLowerCase() &&
+            cognomeCsv === cognome.trim().toLowerCase() &&
+            data?.startsWith(dataOggi)
+          );
+        });
+
+        return res.json({
+          exists: !!target
+        });
+      })
+      .on("error", err => res.status(500).json({ error: err.message }));
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 io.on("connection", socket => {
